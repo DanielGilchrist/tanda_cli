242b5773005bf0d147da6df44fd2d63ba3364643
Daniel Gilchrist <danniieelg@gmail.com>
2022-08-03 21:38:26 +0100

    Add command for seeing time worked today

--- src/cli/parser.cr
+++ src/cli/parser.cr
@@ -16,46 +16,79 @@ module Tanda::CLI
           Representers::Me.new(parsed_response).display
         end
 
-        parser.on("time_worked", "See how many hours you've worked this week") do
+        parser.on("time_worked", "See how many hours you've worked") do
           list = false
 
-          OptionParser.parse do |time_worked_parser|
-            time_worked_parser.on("--list", "List days worked") do
-              list = true
+          parser.on("week", "Time you've worked this week") do
+            OptionParser.parse do |time_worked_parser|
+              time_worked_parser.on("--list", "List days worked") do
+                list = true
+              end
             end
+
+            handle_time_worked_week(list)
           end
 
-          handle_time_worked(list)
+          parser.on("today", "Time you've worked today") do
+            handle_time_worked_today
+          end
         end
       end
     end
 
-    private def handle_time_worked(list : Bool)
+    private def handle_time_worked_today
       now = Time.local
-      start_date, finish_date = [
-        now.at_beginning_of_week,
-        now
+      shifts = request_shifts(now, now)
+      total_time_worked = calculate_time_worked(shifts)
+
+      if total_time_worked.zero?
+        puts "You haven't clocked in today"
+      else
+        puts("You've worked #{total_time_worked.total_hours.to_i} hours and #{total_time_worked.minutes} minutes today")
+      end
+    end
+
+    private def handle_time_worked_week(list : Bool)
+      now = Time.local
+      shifts = request_shifts(now.at_beginning_of_week, now)
+      total_time_worked = calculate_time_worked(shifts, print: list)
+
+      if total_time_worked.zero?
+        puts "You haven't clocked in this week"
+      else
+        puts("You've worked #{total_time_worked.total_hours.to_i} hours and #{total_time_worked.minutes} minutes this week")
+      end
+    end
+
+    private def request_shifts(start_date : Time, finish_date : Time)
+      start_string, finish_string = [
+        start_date,
+        finish_date
       ]
       .map(&.to_s("%Y-%m-%d"))
 
       response = client.get("/shifts", query: {
-        "user_ids" => "66585",
-        "from"     => start_date,
-        "to"       => finish_date
+        "user_ids" => "66585", # TODO: Don't hard-code this
+        "from"     => start_string,
+        "to"       => finish_string
       })
 
+      Array(Types::Shift).from_json(response.body)
+    end
+
+    private def calculate_time_worked(shifts : Array(Types::Shift), print : Bool = false) : Time::Span
       total_time_worked = Time::Span.zero
-      Array(Types::Shift).from_json(response.body).each do |shift|
+      shifts.each do |shift|
         time_worked = shift.time_worked
         worked_so_far = shift.worked_so_far
 
-        print_shift(shift, time_worked, worked_so_far) if list
+        print_shift(shift, time_worked, worked_so_far) if print
 
         total_time = time_worked || worked_so_far
         total_time_worked += total_time if total_time
       end
 
-      puts("Total time worked this week: #{total_time_worked.total_hours.to_i} hours and #{total_time_worked.minutes} minutes")
+      total_time_worked
     end
 
     def print_shift(shift : Types::Shift, time_worked : Time::Span?, worked_so_far : Time::Span?)
