a78568dcb1312c13ffd6a22f4562fa65597f5e32
Daniel Gilchrist <danniieelg@gmail.com>
2022-08-03 23:06:26 +0100

    Better test for compile

--- src/types/converters/time.cr
+++ src/types/converters/time.cr
@@ -5,7 +5,6 @@ module Tanda::CLI
     module Converters::Time
       def self.from_json(value : JSON::PullParser) : ::Time?
         timestamp = value.read_int_or_null
-        return unless timestamp
 
         time = ::Time.unix(timestamp.to_i32)
         time.in(::Time::Location.load("Europe/London")) # TODO: Don't hard-code time zone
