5a744e611f3b236b12bb90876285248975c740ff
Daniel Gilchrist <daniel@workforce.com>
2022-08-06 22:02:26 +0100

    Add "spectator" shard

--- spec/current_spec.cr
+++ spec/current_spec.cr
@@ -1,6 +1,6 @@
 require "./spec_helper"
 
-describe Tanda::CLI::Current do
+Spectator.describe Tanda::CLI::Current do
   context "Setting user" do
     before_each do
       Tanda::CLI::Current.reset!
@@ -8,7 +8,7 @@ describe Tanda::CLI::Current do
 
     it "Raises when Current.user! is called without a user set" do
       expect_raises Tanda::CLI::Current::UserNotSet do
-        puts Tanda::CLI::Current.user!
+        Tanda::CLI::Current.user!
       end
     end
 
@@ -16,7 +16,7 @@ describe Tanda::CLI::Current do
       user = Tanda::CLI::Current::User.new(id: 1, time_zone: "Europe/London")
 
       Tanda::CLI::Current.set_user!(user)
-      Tanda::CLI::Current.user!.should eq(user)
+      expect(Tanda::CLI::Current.user!).to eq(user)
     end
 
     it "Raises when Current.set_user! is called a second time" do
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
-require "spec"
-require "../src/tanda_cli"
+require "../src/*"
+require "spectator"
