0ed662451c9cd6c0f3097d64dc353243da8479ae
Daniel Gilchrist <daniel@workforce.com>
2022-08-06 22:38:15 +0100

    Remove space in shebangs

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,3 +1,3 @@
-#! /bin/bash
+#!/bin/bash
 
 crystal build src/tanda_cli.cr --release --no-debug && mv tanda_cli bin/tanda_cli
--- scripts/run-spec.sh
+++ scripts/run-spec.sh
@@ -1,3 +1,3 @@
-#! /bin/bash
+#!/bin/bash
 
 crystal spec -Dtest --no-debug --order=random --error-on-warnings
