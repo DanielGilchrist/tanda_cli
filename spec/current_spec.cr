cc101ddc567e229e917b98cd22de517720ae5d77
Daniel Gilchrist <daniel@workforce.com>
2022-08-06 22:12:33 +0100

    Refactor Current tests to use let

--- spec/current_spec.cr
+++ spec/current_spec.cr
@@ -1,30 +1,32 @@
 require "./spec_helper"
 
 Spectator.describe Tanda::CLI::Current do
-  context "Setting user" do
-    before_each do
-      Tanda::CLI::Current.reset!
-    end
+  before_each do
+    Tanda::CLI::Current.reset!
+  end
 
-    it "Raises when Current.user! is called without a user set" do
-      expect_raises Tanda::CLI::Current::UserNotSet do
-        Tanda::CLI::Current.user!
+  context "Current user" do
+    context "Retrieving user" do
+      it "Raises when Current.user! is called without a user set" do
+        expect_raises Tanda::CLI::Current::UserNotSet do
+          Tanda::CLI::Current.user!
+        end
       end
     end
 
-    it "Returns set user when calling Current.user!" do
-      user = Tanda::CLI::Current::User.new(id: 1, time_zone: "Europe/London")
+    context "Setting user" do
+      let(user) { Tanda::CLI::Current::User.new(id: 1, time_zone: "Europe/London") }
 
-      Tanda::CLI::Current.set_user!(user)
-      expect(Tanda::CLI::Current.user!).to eq(user)
-    end
-
-    it "Raises when Current.set_user! is called a second time" do
-      user = Tanda::CLI::Current::User.new(id: 1, time_zone: "Europe/London")
-
-      Tanda::CLI::Current.set_user!(user)
-      expect_raises Tanda::CLI::Current::UserAlreadySet do
+      it "Returns set user when calling Current.user!" do
         Tanda::CLI::Current.set_user!(user)
+        expect(Tanda::CLI::Current.user!).to eq(user)
+      end
+
+      it "Raises when Current.set_user! is called a second time" do
+        Tanda::CLI::Current.set_user!(user)
+        expect_raises Tanda::CLI::Current::UserAlreadySet do
+          Tanda::CLI::Current.set_user!(user)
+        end
       end
     end
   end
