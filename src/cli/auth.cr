dc50c4722c049fd35b0a9693da24f3e299abc0a9
Daniel Gilchrist <daniel@workforce.com>
2022-10-23 05:01:03 +0100

    try_get_input -> try_get_input!

--- src/cli/auth.cr
+++ src/cli/auth.cr
@@ -4,7 +4,7 @@ module Tanda::CLI
 
     def self.request_user_information! : Tuple(String, String, String)
       valid_site_prefixes = VALID_SITE_PREFIXES.join(", ")
-      site_prefix = try_get_input(message: "Site prefix (#{valid_site_prefixes}):", error_prefix: "Site prefix")
+      site_prefix = try_get_input!(message: "Site prefix (#{valid_site_prefixes}):", error_prefix: "Site prefix")
 
       unless VALID_SITE_PREFIXES.includes?(site_prefix)
         Utils::Display.error("Invalid site prefix")
@@ -12,16 +12,16 @@ module Tanda::CLI
         exit
       end
 
-      email = try_get_input(message: "Whats your email?", error_prefix: "Email")
+      email = try_get_input!(message: "Whats your email?", error_prefix: "Email")
 
       password = STDIN.noecho do
-        try_get_input(message: "What's your password?", error_prefix: "Password")
+        try_get_input!(message: "What's your password?", error_prefix: "Password")
       end
 
       {site_prefix, email, password}
     end
 
-    private def self.try_get_input(message : String, error_prefix : String) : String
+    private def self.try_get_input!(message : String, error_prefix : String) : String
       puts "#{message}\n"
       input = gets.try(&.chomp).presence || handle_invalid_input!("#{error_prefix} cannot be blank")
       puts ""
