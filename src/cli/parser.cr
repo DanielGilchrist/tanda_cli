42614a0cb5538ac8dc9ed79f3228944b79a72aa6
Daniel Gilchrist <danniieelg@gmail.com>
2022-10-23 02:16:46 +0100

    Clean up `cli/parser` (#20)

--- src/cli/parser.cr
+++ src/cli/parser.cr
@@ -21,20 +21,20 @@ module Tanda::CLI
         end
 
         parser.on("time_worked", "See how many hours you've worked") do
+          display = false
+
+          OptionParser.parse do |time_worked_parser|
+            time_worked_parser.on("--display", "Show shift/s") do
+              display = true
+            end
+          end
+
           parser.on("today", "Time you've worked today") do
-            handle_time_worked_today
+            CLI::Commands::TimeWorked::Today.new(client, display).execute
           end
 
           parser.on("week", "Time you've worked this week") do
-            list = false
-
-            OptionParser.parse do |time_worked_parser|
-              time_worked_parser.on("--list", "List days worked") do
-                list = true
-              end
-            end
-
-            handle_time_worked_week(list)
+            CLI::Commands::TimeWorked::Week.new(client, display).execute
           end
         end
 
@@ -62,52 +62,6 @@ module Tanda::CLI
           CLI::Commands::CurrentUser.new(client, config, new_id_or_name).execute
         end
       end
-    end
-
-    private def handle_time_worked_today
-      now = Time.local
-      shifts = client.shifts(now)
-      total_time_worked = calculate_time_worked(shifts)
-
-      if total_time_worked.zero?
-        puts "You haven't clocked in today"
-      else
-        puts("You've worked #{total_time_worked.total_hours.to_i} hours and #{total_time_worked.minutes} minutes today")
-      end
-    end
-
-    private def handle_time_worked_week(list : Bool)
-      now = Time.local
-      shifts = client.shifts(now.at_beginning_of_week, now)
-      total_time_worked = calculate_time_worked(shifts, print: list)
-
-      if total_time_worked.zero?
-        puts "You haven't clocked in this week"
-      else
-        puts("You've worked #{total_time_worked.total_hours.to_i} hours and #{total_time_worked.minutes} minutes this week")
-      end
-    end
-
-    private def calculate_time_worked(shifts : Array(Types::Shift), print : Bool = false) : Time::Span
-      total_time_worked = Time::Span.zero
-      shifts.each do |shift|
-        time_worked = shift.time_worked
-        worked_so_far = shift.worked_so_far
-
-        print_shift(shift, time_worked, worked_so_far) if print
-
-        total_time = time_worked || worked_so_far
-        total_time_worked += total_time if total_time
-      end
-
-      total_time_worked
-    end
-
-    private def print_shift(shift : Types::Shift, time_worked : Time::Span?, worked_so_far : Time::Span?)
-      time_worked && puts "Time worked: #{time_worked.hours} hours and #{time_worked.minutes} minutes"
-      (!time_worked && worked_so_far) && puts "Worked so far: #{worked_so_far.hours} hours and #{worked_so_far.minutes} minutes"
-
-      Representers::Shift.new(shift).display
     end
 
     private getter client : API::Client
