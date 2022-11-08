9c94b739b1b74a9c6209c911ca4060b06e5aa443
Daniel Gilchrist <danniieelg@gmail.com>
2022-11-08 23:41:16 +0000

    Fix issue where we'd always create a default config even if one already exists

--- src/tanda_cli.cr
+++ src/tanda_cli.cr
@@ -11,23 +11,8 @@ require "./cli/**"
 module Tanda::CLI
   extend self
 
-  def try_parse_config!(config : Configuration)
-    config.parse_config!
-  rescue error
-    {% if flag?(:debug) %}
-      raise(error)
-    {% else %}
-      value = error.message.try(&.split("\n").first) if error.is_a?(JSON::SerializableError)
-      reason = " (#{value})" if value
-      Utils::Display.error("Invalid Config#{reason}")
-      exit
-    {% end %}
-  end
-
   def main
-    config = Configuration.new
-    try_parse_config!(config)
-
+    config = Configuration.init
     CLI::Parser.new(config).parse!
   end
 end
