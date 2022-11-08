9592c6004e6557c9a4f9bbef94b9dc95d122974d
Daniel Gilchrist <danniieelg@gmail.com>
2022-11-08 16:57:11 +0000

    Remove unnecessary intermediate object

--- src/api/result.cr
+++ src/api/result.cr
@@ -3,33 +3,27 @@ require "../types/error"
 module Tanda::CLI
   module API
     class Result(T)
-      class Matcher(T)
-        def initialize(@value : T | Types::Error); end
-
-        def ok(&block : T -> ::Nil)
-          value = self.value
-          return if value.is_a?(Types::Error)
-
-          yield(value)
-        end
-
-        def error(&block : Types::Error -> ::Nil)
-          value = self.value
-          return unless value.is_a?(Types::Error)
-
-          yield(value)
-        end
-
-        private getter value : T | Types::Error
-      end
-
       def initialize(@value : T | Types::Error); end
 
       def match(&block)
-        with Matcher(T).new(value) yield
+        with self yield
       end
 
       private getter value : T | Types::Error
+
+      private def ok(&block : T -> ::Nil)
+        value = self.value
+        return if value.is_a?(Types::Error)
+
+        yield(value)
+      end
+
+      private def error(&block : Types::Error -> ::Nil)
+        value = self.value
+        return unless value.is_a?(Types::Error)
+
+        yield(value)
+      end
     end
   end
 end
