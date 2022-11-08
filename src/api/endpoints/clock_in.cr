51ed6a65184c73cb87fd2308923d30814d051613
Daniel Gilchrist <danniieelg@gmail.com>
2022-11-08 16:50:13 +0000

    Use new API for clockin

--- src/api/endpoints/clock_in.cr
+++ src/api/endpoints/clock_in.cr
@@ -6,15 +6,15 @@ module Tanda::CLI
     module Endpoints::ClockIn
       include Endpoints::Interface
 
-      def send_clock_in(time : Time, type : String) : Types::Error?
+      def send_clock_in(time : Time, type : String) : API::Result(Nil)
         response = post("/clockins", body: {
           "time" => time.to_unix.to_s,
           "type" => type,
           "user_id" => Current.user.id.to_s
         })
-        return if response.success?
 
-        Types::Error.from_json(response.body)
+        result = Types::Error.from_json(response.body) unless response.success?
+        API::Result(Nil).new(result)
       end
     end
   end
