d239d948e23267d3e5d90e8b0e9f63da97250903
Daniel Gilchrist <danniieelg@gmail.com>
2022-11-12 15:35:57 +0000

    Add tanda_cli clockin display command to list current clockins (#36)

---
+++ src/representers/clock_in.cr
@@ -0,0 +1,15 @@
+require "./base"
+require "../types/clock_in"
+
+module Tanda::CLI
+  module Representers
+    class ClockIn < Base(Types::ClockIn)
+      def display
+        display_with_padding("ID", object.id)
+        display_with_padding("Time", object.time)
+        display_with_padding("Type", object.type)
+        puts "\n"
+      end
+    end
+  end
+end
