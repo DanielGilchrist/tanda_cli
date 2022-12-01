bb277724ef6394a4a788d2de00531700c7d18947
Daniel Gilchrist <daniel@workforce.com>
2022-12-01 20:49:12 +0000

    Use -o compiler command

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-crystal build src/tanda_cli.cr --release --no-debug --progress && mv tanda_cli bin/tanda_cli
+crystal build src/tanda_cli.cr --release --no-debug --progress -o bin/tanda_cli
