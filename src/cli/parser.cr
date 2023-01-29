68d65e51db8296855868a43e19b9f9157a522b32
Daniel Gilchrist <danniieelg@gmail.com>
2023-01-29 14:49:55 +0000

    Move logic in parser to mixin

--- src/cli/parser.cr
+++ src/cli/parser.cr
@@ -1,4 +1,5 @@
 require "./commands/**"
+require "./parser/helpers"
 require "../current"
 require "../api/client"
 require "../representers/**"
@@ -6,7 +7,7 @@ require "../types/**"
 
 module Tanda::CLI
   class CLI::Parser
-    @client : API::Client? = nil
+    include CLI::Parser::Helpers
 
     def initialize(@config : Configuration, @args = ARGV); end
 
@@ -78,71 +79,6 @@ module Tanda::CLI
           exit
         end
       end
-    end
-
-    private def build_client_with_current_user : API::Client
-      @client ||= begin
-        client = create_client_from_config
-        CLI::CurrentUser.new(client, config).set!
-
-        client
-      end
-    end
-
-    private def maybe_display_staging_warning
-      return unless config.staging?
-
-      message = begin
-        if (mode = config.mode) != "staging"
-          "Command running on #{mode}"
-        else
-          "Command running in staging mode"
-        end
-      end
-
-      Utils::Display.warning(message)
-    end
-
-    private def create_client_from_config : API::Client
-      token = config.access_token.token
-
-      # if a token can't be parsed from the config, get username and password from user and request a token
-      if token.nil?
-        fetch_new_token!
-        return create_client_from_config
-      end
-
-      url = config.api_url
-      API::Client.new(url, token)
-    end
-
-    private def fetch_new_token!
-      site_prefix, email, password = CLI::Auth.request_user_information!
-
-      auth_site_prefix = begin
-        if config.staging?
-          case site_prefix
-          when "my"
-            "staging"
-          when "eu"
-            "staging.eu"
-          when "us"
-            "staging.us"
-          end
-        end
-      end || site_prefix
-
-      access_token = API::Auth.fetch_access_token!(auth_site_prefix, email, password).or do |error|
-        Utils::Display.error!("Unable to authenticate (likely incorrect login details)") do |sub_errors|
-          sub_errors << "Error Type: #{error.error}\n"
-
-          description = error.error_description
-          sub_errors << "Message: #{description}" if description
-        end
-      end
-
-      Utils::Display.success("Retrieved token!#{config.staging? ? " (staging)" : ""}\n")
-      config.overwrite!(site_prefix, email, access_token)
     end
   end
 end
