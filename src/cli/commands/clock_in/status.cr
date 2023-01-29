98ad7e59a23de98e90af95f2813e53fcd80e24f6
Daniel Gilchrist <danniieelg@gmail.com>
2023-01-29 18:54:03 +0000

    Add `DetermineStatus` mixin

--- src/cli/commands/clock_in/status.cr
+++ src/cli/commands/clock_in/status.cr
@@ -1,46 +1,72 @@
+require "./determine_status"
+
 module Tanda::CLI
   module CLI::Commands
     class ClockIn
       class Status
+        include ClockIn::DetermineStatus
+
+        @clockins : Array(Types::ClockIn)? = nil
+
         def initialize(@client : API::Client); end
 
-        # TODO: Fix lazy logic
-        # The logic here should match the status logic in ClockInValidator in src/cli/commands/clock_in.cr
-        # to handle the case where clock ins aren't in a sound order
         def execute
-          now = Utils::Time.now
-          clockins = client.clockins(now).or(&.display!)
-          clockin = clockins.sort_by(&.time).last?
-          return puts "You aren't currently clocked in" if clockin.nil?
+          reversed_clockins = clockins.sort_by(&.time).reverse!
 
-          case clockin.type
-          in Types::ClockIn::Type::Start
-            display_clocked_in
-            puts "You clocked in at #{clockin.pretty_date_time}"
-          in Types::ClockIn::Type::Finish
-            display_clocked_out
-            puts "You clocked out at #{clockin.pretty_date_time}"
-          in Types::ClockIn::Type::BreakStart
-            display_on_break
-            puts "You started a break at #{clockin.pretty_date_time}"
-          in Types::ClockIn::Type::BreakFinish
-            display_clocked_in
-            puts "You finished your break at #{clockin.pretty_date_time}"
+          case determine_status
+          in ClockInStatus::ClockedIn
+            handle_clocked_in(reversed_clockins)
+          in ClockInStatus::ClockedOut
+            handle_clocked_out(reversed_clockins)
+          in ClockInStatus::BreakStarted
+            handle_break_started(reversed_clockins)
           end
         end
 
         private getter client
 
-        private def display_clocked_in
+        private def handle_clocked_in(clockins : Array(Types::ClockIn))
           puts "You are clocked in"
+
+          finished_break = clockins.find(&.type.==(Types::ClockIn::Type::BreakFinish))
+          if finished_break
+            puts "You finished your break at #{finished_break.pretty_date_time}"
+          else
+            latest_clockin = clockins.find(&.type.==(Types::ClockIn::Type::Start))
+
+            if latest_clockin.nil?
+              Utils::Display.fatal!("Clock in status is clocked in but clock in can't be found!")
+            end
+
+            puts "You clocked in at #{latest_clockin.pretty_date_time}"
+          end
         end
 
-        private def display_clocked_out
+        private def handle_clocked_out(clockins : Array(Types::ClockIn))
           puts "You are clocked out"
+
+          latest_clockout = clockins.find(&.type.==(Types::ClockIn::Type::Finish))
+          if latest_clockout
+            puts "You clocked out at #{latest_clockout.pretty_date_time}"
+          else
+            puts "You aren't currently clocked in"
+          end
         end
 
-        private def display_on_break
+        private def handle_break_started(clockins : Array(Types::ClockIn))
           puts "You are on break"
+
+          latest_break_start = clockins.find(&.type.==(Types::ClockIn::Type::BreakStart))
+
+          if latest_break_start.nil?
+            Utils::Display.fatal!("Clock in status is break start but break start can't be found!")
+          end
+
+          puts "You started a break at #{latest_break_start.pretty_date_time}"
+        end
+
+        private def clockins : Array(Types::ClockIn)
+          @clockins ||= client.clockins(Utils::Time.now).or(&.display!)
         end
       end
     end
