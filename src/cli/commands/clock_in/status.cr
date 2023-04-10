9144bc0696942fc9134778b7b609012d0bb0c5ed
Daniel Gilchrist <danniieelg@gmail.com>
2023-04-10 01:23:02 +0100

    Refactor clockin status command (#72)

--- src/cli/commands/clock_in/status.cr
+++ src/cli/commands/clock_in/status.cr
@@ -1,64 +1,43 @@
-require "./determine_status"
-
 module Tanda::CLI
   module CLI::Commands
     class ClockIn
       class Status
-        include ClockIn::DetermineStatus
-
         def initialize(@client : API::Client); end
 
         def execute
-          reversed_clockins = clockins.sort_by(&.time).reverse!
+          todays_shifts = client.todays_shifts.or(&.display!).sort_by(&.id)
+          ongoing_shift = todays_shifts.reverse_each.find(&.ongoing?)
+          return handle_ongoing_shift(ongoing_shift) if ongoing_shift
 
-          case determine_status
-          in ClockInStatus::ClockedIn
-            handle_clocked_in(reversed_clockins)
-          in ClockInStatus::ClockedOut
-            handle_clocked_out(reversed_clockins)
-          in ClockInStatus::BreakStarted
-            handle_break_started(reversed_clockins)
-          end
+          last_shift = todays_shifts.last?
+          return puts "You aren't currently clocked in" if last_shift.nil?
+
+          handle_clocked_out(last_shift)
         end
 
         private getter client
 
-        private def handle_clocked_in(clockins : Array(Types::ClockIn))
-          puts "You are clocked in"
+        def handle_ongoing_shift(shift : Types::Shift)
+          if !(shift_breaks = shift.breaks).empty?
+            ongoing_breaks, finished_breaks = shift_breaks.partition(&.ongoing?)
+            if ongoing_break = ongoing_breaks.last?
+              puts "You are on break"
+              puts "You started a break at #{ongoing_break.pretty_start_time}"
+            else
+              finished_break = finished_breaks.last
 
-          if finished_break = clockins.find(&.type.==(Types::ClockIn::Type::BreakFinish))
-            puts "You finished your break at #{finished_break.pretty_date_time}"
-          else
-            if (latest_clockin = clockins.find(&.type.==(Types::ClockIn::Type::Start))).nil?
-              Utils::Display.fatal!("Clock in status is clocked in but clock in can't be found!")
+              puts "You are clocked in"
+              puts "You finished a break at #{finished_break.pretty_finish_time}"
             end
-
-            puts "You clocked in at #{latest_clockin.pretty_date_time}"
-          end
-        end
-
-        private def handle_clocked_out(clockins : Array(Types::ClockIn))
-          puts "You are clocked out"
-
-          if latest_clockout = clockins.find(&.type.==(Types::ClockIn::Type::Finish))
-            puts "You clocked out at #{latest_clockout.pretty_date_time}"
           else
-            puts "You aren't currently clocked in"
+            puts "You are clocked in"
+            puts "You clocked in at #{shift.pretty_start_time}"
           end
         end
 
-        private def handle_break_started(clockins : Array(Types::ClockIn))
-          puts "You are on break"
-
-          if (latest_break_start = clockins.find(&.type.==(Types::ClockIn::Type::BreakStart))).nil?
-            Utils::Display.fatal!("Clock in status is break start but break start can't be found!")
-          end
-
-          puts "You started a break at #{latest_break_start.pretty_date_time}"
-        end
-
-        private def clockins : Array(Types::ClockIn)
-          client.clockins(Utils::Time.now).or(&.display!)
+        def handle_clocked_out(last_shift : Types::Shift)
+          puts "You are clocked out"
+          puts "You clocked out at #{last_shift.pretty_finish_time}"
         end
       end
     end
