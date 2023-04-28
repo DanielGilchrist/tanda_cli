b4c2a8ff1b196c687039611b069ea75f0b759217
Daniel Gilchrist <danniieelg@gmail.com>
2023-04-28 21:55:07 +0100

    DRY up `API::Result(T)`

--- src/api/result.cr
+++ src/api/result.cr
@@ -3,30 +3,21 @@ require "../types/error"
 module Tanda::CLI
   module API
     class Result(T)
-      def self.from(response : HTTP::Client::Response) : API::Result(T)
-        {% if T < JSON::Serializable %}
-          # Allow types that include `JSON::Serializable`
-        {% elsif T.has_method?(:to_json) %}
-          # This accounts for stdlib types like `Array` that are "serializable"
-        {% else %}
-          # If above conditions aren't met, throw a compiler error
-          {{ raise "Unsupported type #{T}" }}
-        {% end %}
-
-        result = begin
-          {% if T == Nil %}
-            # Special case - if we don't care about a successful response's value we use Nil
-            response.success? ? nil : Types::Error.from_json(response.body)
-          {% else %}
-            (response.success? ? T : Types::Error).from_json(response.body)
-          {% end %}
-        end
-
-        new(result)
+      def self.from(response : HTTP::Client::Response) : self
+        new(parse(response))
       end
 
       # allows additional processing on a successful response
-      def self.from(response : HTTP::Client::Response, & : T -> T | Types::Error) : API::Result(T)
+      def self.from(response : HTTP::Client::Response, & : T -> T | Types::Error) : self
+        case result = parse(response)
+        in Types::Error
+          new(result)
+        in T
+          new(yield(result))
+        end
+      end
+
+      private def self.parse(response : HTTP::Client::Response) : T | Types::Error
         {% if T < JSON::Serializable %}
           # Allow types that include `JSON::Serializable`
         {% elsif T.has_method?(:to_json) %}
@@ -36,10 +27,12 @@ module Tanda::CLI
           {{ raise "Unsupported type #{T}" }}
         {% end %}
 
-        result = (response.success? ? T : Types::Error).from_json(response.body)
-        return new(result) if result.is_a?(Types::Error)
-
-        new(yield(result))
+        {% if T == Nil %}
+          # Special case - if we don't care about a successful response's value we use Nil
+          response.success? ? nil : Types::Error.from_json(response.body)
+        {% else %}
+          (response.success? ? T : Types::Error).from_json(response.body)
+        {% end %}
       end
 
       # This class should only be initialized with the `from` class method
