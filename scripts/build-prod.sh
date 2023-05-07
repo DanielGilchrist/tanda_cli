d3b988d13c302f8f92a93107004e2551198e20d5
Daniel Gilchrist <danniieelg@gmail.com>
2023-05-07 21:15:03 +0100

    Add a comment about where the script expects to be run from

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script assumes you're running it from the root of the project
+# i.e. ./scripts/build-prod.sh
+
 crystal build src/tanda_cli.cr --release --no-debug --progress --stats \
   && mv tanda_cli bin/tanda_cli \
   && echo \
