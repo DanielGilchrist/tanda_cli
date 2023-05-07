746cc4ca15e1754c7057452ec8bd8cd7aa15a89e
Daniel Gilchrist <danniieelg@gmail.com>
2023-05-07 21:13:22 +0100

    Add success message to build prod script

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-crystal build src/tanda_cli.cr --release --no-debug --progress --stats && mv tanda_cli bin/tanda_cli
+crystal build src/tanda_cli.cr --release --no-debug --progress --stats \
+  && mv tanda_cli bin/tanda_cli \
+  && echo \
+  && printf "\e[32mSuccess:\e[0m compiled release binary to $(pwd)/bin/tanda_cli\n"
