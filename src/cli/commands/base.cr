fab37a06133223a15e6b8bd68fef1e5d7047fbca
Daniel Gilchrist <daniel@workforce.com>
2023-09-18 14:32:25 +0100

    Add error handling for incorrect/unknown options/arguments

--- src/cli/commands/base.cr
+++ src/cli/commands/base.cr
@@ -28,6 +28,47 @@ module Tanda::CLI
           before_run(arguments, options)
         end
       end
+
+      # A hook method for when the command raises an exception during execution
+      def on_error(ex : Exception)
+        {% if flag?(:debug) %}
+          super
+        {% else %}
+          Utils::Display.error!("Something went wrong: #{ex.message}")
+        {% end %}
+      end
+
+      # A hook method for when the command receives missing arguments during execution
+      def on_missing_arguments(arguments : Array(String))
+        Utils::Display.error("Missing required argument#{"s" if arguments.size > 1}: #{arguments.join(", ")}")
+        puts help_template
+      end
+
+      # A hook method for when the command receives unknown arguments during execution
+      def on_unknown_arguments(arguments : Array(String))
+        Utils::Display.error("Unknown argument#{"s" if arguments.size > 1}: #{arguments.join(", ")}")
+        puts help_template
+      end
+
+      # A hook method for when the command receives an invalid option, for example, a value given to
+      # an option that takes no arguments
+      def on_invalid_option(message : String)
+        Utils::Display.error(message)
+        puts help_template
+      end
+
+      # A hook method for when the command receives missing options that are required during
+      # execution. By default, this raises an `CommandError`.
+      def on_missing_options(options : Array(String))
+        Utils::Display.error("Missing required option#{"s" if options.size > 1}: #{options.join(", ")}")
+        puts help_template
+      end
+
+      # A hook method for when the command receives unknown options during execution
+      def on_unknown_options(options : Array(String))
+        Utils::Display.error("Unknown option#{"s" if options.size > 1}: #{options.join(", ")}")
+        puts help_template
+      end
     end
   end
 end
