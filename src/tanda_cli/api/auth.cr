848cd6912b594a3093136fb095529040b6197736
Daniel Gilchrist <danniieelg@gmail.com>
2023-10-13 19:43:28 +0100

    Move request_user_information! to where it's used

--- src/tanda_cli/api/auth.cr
+++ src/tanda_cli/api/auth.cr
@@ -11,9 +11,11 @@ module TandaCLI
     module Auth
       extend self
 
+      VALID_SITE_PREFIXES = {"my", "eu", "us"}
+
       def fetch_new_token!
         config = Current.config
-        site_prefix, email, password = TandaCLI::Auth.request_user_information!
+        site_prefix, email, password = request_user_information!
 
         auth_site_prefix = begin
           if config.staging?
@@ -94,6 +96,44 @@ module TandaCLI
           "platform",
         }
           .join(" ")
+      end
+
+      private def request_user_information! : Tuple(String, String, String)
+        valid_site_prefixes = VALID_SITE_PREFIXES.join(", ")
+        site_prefix = request_site_prefix(message: "Site prefix (#{valid_site_prefixes} - Default is \"my\"):")
+
+        unless VALID_SITE_PREFIXES.includes?(site_prefix)
+          Utils::Display.error!("Invalid site prefix") do |sub_errors|
+            sub_errors << "Site prefix must be one of #{valid_site_prefixes}"
+          end
+        end
+        puts
+
+        email = try_request_input_with_error!(message: "Whats your email?", error_prefix: "Email")
+        puts
+
+        password = STDIN.noecho do
+          try_request_input_with_error!(message: "What's your password?", error_prefix: "Password")
+        end
+        puts
+
+        {site_prefix, email, password}
+      end
+
+      private def try_request_input_with_error!(message : String, error_prefix : String) : String
+        try_request_input(message: message) || Utils::Display.error!("#{error_prefix} cannot be blank")
+      end
+
+      private def request_site_prefix(message : String) : String
+        input = try_request_input(message: message)
+        (input || "my").tap do
+          Utils::Display.warning("Defaulting to \"my\"") if input.nil?
+        end
+      end
+
+      private def try_request_input(message : String) : String?
+        puts "#{message}"
+        gets.try(&.chomp).presence
       end
     end
   end
