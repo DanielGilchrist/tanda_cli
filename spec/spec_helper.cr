892aefa199aab7271b1234977d22dbab1c4f74a5
Daniel Gilchrist <daniel@workforce.com>
2023-10-27 11:26:11 +0100

    Add webmock

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,9 +1,12 @@
 require "spectator"
+require "webmock"
+
 require "../src/tanda_cli"
 
 Spectator.configure do |config|
   config.randomize = true
   config.before_each do
     TandaCLI::Current.reset!
+    WebMock.reset
   end
 end
