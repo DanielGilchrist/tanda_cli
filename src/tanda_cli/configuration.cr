5ed4f3a9a39f658230154ced059c008bb9c44717
Daniel Gilchrist <danniieelg@gmail.com>
2023-11-22 23:19:05 +0000

    Add `Environment#current_organisation?`

--- src/tanda_cli/configuration.cr
+++ src/tanda_cli/configuration.cr
@@ -62,7 +62,7 @@ module TandaCLI
     environment_property site_prefix : String
     environment_property access_token : AccessToken
 
-    delegate clear_access_token!, current_organisation!, to: current_environment
+    delegate clear_access_token!, current_organisation?, current_organisation!, to: current_environment
 
     def pretty_start_of_week : String
       @start_of_week.to_s
