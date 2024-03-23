900449131335f18aac20574a8a6fff4b035099f6
Daniel Gilchrist <danniieelg@gmail.com>
2024-03-23 15:42:26 +0000

    Fix borked offset option for time worked

--- src/tanda_cli/commands/time_worked/week.cr
+++ src/tanda_cli/commands/time_worked/week.cr
@@ -12,12 +12,12 @@ module TandaCLI
           @summary = @description = "Show time worked for a week"
 
           add_option 'd', "display", description: "Print Shift"
-          add_option 'o', "offset", description: "Offset from today"
+          add_option 'o', "offset", type: :single, required: false, description: "Offset from today"
         end
 
         def run_(arguments : Cling::Arguments, options : Cling::Options) : Nil
           display = options.has?("display")
-          offset = options.get?("offset").try(&.as_i32)
+          offset = options.get?("offset").try(&.as_s.to_i32?)
 
           Executors::TimeWorked::Week.new(client, display, offset).execute
         end
