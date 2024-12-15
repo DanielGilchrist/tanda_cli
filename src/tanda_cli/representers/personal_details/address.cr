c69ab55b8ea4cafd91ac8d8b48dee7917ee6d851
Daniel Gilchrist <danniieelg@gmail.com>
2024-12-15 01:23:09 +0000

    Avoid an unnecessary allocation
    Original is 1.42x slower, presumably because it originally allocates and
    then allocates a second Array with `compact`. Interestingly `compact!`
    which allocates the original Array then mutates is 1.16x slower than
    `Tuple#reject(Nil)`

--- src/tanda_cli/representers/personal_details/address.cr
+++ src/tanda_cli/representers/personal_details/address.cr
@@ -6,14 +6,14 @@ module TandaCLI
     class PersonalDetails
       class Address < Base(Types::PersonalDetails::Address)
         private def build_display(builder : String::Builder)
-          address_string = [
+          address_string = {
             @object.street_line_one,
             @object.street_line_two,
             @object.city,
             @object.state,
             @object.postcode,
             @object.country,
-          ].compact.join(", ")
+          }.reject(Nil).join(", ")
 
           builder << address_string if address_string.presence
         end
