994164513e54e64551ffb3cd42da3672f4a2466d
Daniel Gilchrist <daniel@workforce.com>
2024-12-22 20:15:45 +0000

    format

--- spec/tanda_cli_spec.cr
+++ spec/tanda_cli_spec.cr
@@ -4,10 +4,10 @@ describe TandaCLI do
   context "Main" do
     it "Running main with no arguments passes" do
       # TODO: Make running main testable
-      #io = IO::Memory.new
-      #TandaCLI.main([] of String, io)
+      # io = IO::Memory.new
+      # TandaCLI.main([] of String, io)
       #
-      #io.to_s.should contain("A CLI application for people using Tanda/Workforce.com")
+      # io.to_s.should contain("A CLI application for people using Tanda/Workforce.com")
     end
   end
 end
