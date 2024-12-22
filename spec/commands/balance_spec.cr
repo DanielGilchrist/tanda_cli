3cde0d385a42199c8e224eb97992e0ee0659e5cc
Daniel Gilchrist <daniel@workforce.com>
2024-12-22 21:21:50 +0000

    Format

--- spec/commands/balance_spec.cr
+++ spec/commands/balance_spec.cr
@@ -4,27 +4,27 @@ describe TandaCLI::Commands::Balance do
   it "Fetches and displays leave balance info" do
     WebMock
       .stub(:get, endpoint("/leave_balances", {
-        "user_ids" => "1"
+        "user_ids" => "1",
       }))
       .to_return(
         status: 200,
         body: [
           {
-            id: 1,
-            user_id: 1,
-            leave_type: "Holiday Leave",
-            hours: 128.0,
+            id:            1,
+            user_id:       1,
+            leave_type:    "Holiday Leave",
+            hours:         128.0,
             should_accrue: true,
-            updated_at: 1704219040
+            updated_at:    1704219040,
           },
           {
-            id: 2,
-            user_id: 1,
-            leave_type: "Sick Leave",
-            hours: 40.0,
+            id:            2,
+            user_id:       1,
+            leave_type:    "Sick Leave",
+            hours:         40.0,
             should_accrue: true,
-            updated_at: 1704219040
-          }
+            updated_at:    1704219040,
+          },
         ]
           .to_json,
       )
