19918a9050b4e395674dd9a5499691d15ef0ef9f
Daniel Gilchrist <daniel@workforce.com>
2024-12-22 21:20:54 +0000

    Add test for balance command

---
+++ spec/commands/balance_spec.cr
@@ -0,0 +1,44 @@
+require "../spec_helper"
+
+describe TandaCLI::Commands::Balance do
+  it "Fetches and displays leave balance info" do
+    WebMock
+      .stub(:get, endpoint("/leave_balances", {
+        "user_ids" => "1"
+      }))
+      .to_return(
+        status: 200,
+        body: [
+          {
+            id: 1,
+            user_id: 1,
+            leave_type: "Holiday Leave",
+            hours: 128.0,
+            should_accrue: true,
+            updated_at: 1704219040
+          },
+          {
+            id: 2,
+            user_id: 1,
+            leave_type: "Sick Leave",
+            hours: 40.0,
+            should_accrue: true,
+            updated_at: 1704219040
+          }
+        ]
+          .to_json,
+      )
+
+    context = Command.run(["balance"])
+
+    expected = <<-OUTPUT
+    Leave Balance
+        ⏳ 128.0 hours
+        🌴 Holiday Leave
+
+
+    OUTPUT
+
+    context.io.to_s.should eq(expected)
+  end
+end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -15,6 +15,9 @@ end
 
 BASE_URI = "https://fakeurlthisisfakenotrealahhhh.com/api/v2"
 
-def endpoint(path)
-  "#{BASE_URI}#{path}"
+def endpoint(path, query = nil)
+  uri = URI.parse("#{BASE_URI}#{path}")
+  uri.query = URI::Params.encode(query) if query
+
+  uri.to_s
 end
