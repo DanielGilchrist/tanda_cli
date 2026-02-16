3f8f6315de21437fbefd1c32056f24ac59589bd2
Daniel Gilchrist <daniel@workforce.com>
2026-02-16 18:14:06 +0000

    fixup! Wrap `String::Builder` for better ergonomics

--- src/tanda_cli/representers/builder.cr
+++ src/tanda_cli/representers/builder.cr
@@ -9,16 +9,6 @@ module TandaCLI
 
       delegate :<<, to: @builder
 
-      def pad_end(char : Char, n = 1)
-        n.times do
-          char.bytes.each do |byte|
-            @builder.chomp!(byte)
-          end
-        end
-
-        n.times { @builder << char }
-      end
-
       def to_s(io)
         io << @builder.to_s
       end
